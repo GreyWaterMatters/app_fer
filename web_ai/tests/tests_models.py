252f62d61337bceaa8025bd9a74c7758eeac26d5
GreyWatterMatters <lorene.meslin@gmail.com>
2023-01-19 12:34:12 +0100

    unit test for user history creation

--- web_ai/tests/tests_models.py
+++ web_ai/tests/tests_models.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from accounts.models import AppUser, History
 from django.contrib.auth import authenticate
+from accounts.functions import preprocess_image
+import cv2
 
 
 # Create your tests here.
@@ -27,6 +29,27 @@ class UserTest(TestCase):
         self.assertFalse((user is not None) and user.is_authenticated)
 
 
+class HistoryTest(TestCase):
 
+    def setUp(self):
+        self.user = AppUser.objects.create_user(username="unit_test", email="unit_test@test.com",
+                                                password="p@sSword123")
+        self.user.save()
 
-#history_test = History.objects.create(name="test_file.png", file_path="file/test_file.png", prediction="", true_prediction="", user=user_test)
+        image = cv2.imread("/home/greywater/Documents/Kirae/app/image_test/2022-11-29_18-11.png", cv2.IMREAD_UNCHANGED)
+        image_bytes = cv2.imencode('.png', image)[1].tobytes()
+        self.prediction = preprocess_image(image_bytes)
+        self.history = History(name="test_file.png", file_path="file/test_file.png", prediction=self.prediction[0],
+                               true_prediction="Angry", user=self.user)
+
+        self.history.save()
+
+    def tearDown(self):
+        self.history.delete()
+        self.user.delete()
+
+    def test_history_creation(self):
+        history_test = History.objects.get(id=self.history.id)
+        self.assertEqual(history_test.true_prediction, "Angry")
+        self.assertEqual(history_test.prediction, self.prediction[0])
+
